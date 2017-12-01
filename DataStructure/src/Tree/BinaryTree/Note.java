489f07895a4db93433603f5a56183568525e9959
mr.c <crh1028vip@163.com>
2017-12-01 22:54:00 +0800

    二叉树

---
+++ DataStructure/src/Tree/BinaryTree/Note.java
@@ -0,0 +1,16 @@
+package Tree.BinaryTree;
+
+/*
+* 二叉树 节点
+* */
+public class Note {
+    public long value;
+
+    public  Note leftNote;
+
+    public Note rightNote;
+
+    Note(long value){
+        this.value=value;
+    }
+}
