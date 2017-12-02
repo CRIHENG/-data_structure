d2b6167b88cbc6508c7acf322c1e5e224f1579c1
mr.c <crh1028vip@163.com>
2017-12-02 21:03:48 +0800

    二叉树

--- DataStructure/src/Tree/BinaryTree/BinaryTreeImp.java
+++ DataStructure/src/Tree/BinaryTree/BinaryTreeImp.java
@@ -4,10 +4,14 @@ public class BinaryTreeImp {
     public Note root;
 
     public void insert(long value) {
+
+
         if (root != null) {
             root.value = value;
             return;
         }
+
+
 
     }
 }
