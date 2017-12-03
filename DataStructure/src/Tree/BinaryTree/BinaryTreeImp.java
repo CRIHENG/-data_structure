4bf7354b84a659e233cbb8c42a707e6d9803346e
mr.c <crh1028vip@163.com>
2017-12-03 23:51:13 +0800

    二叉树

--- DataStructure/src/Tree/BinaryTree/BinaryTreeImp.java
+++ DataStructure/src/Tree/BinaryTree/BinaryTreeImp.java
@@ -4,11 +4,29 @@ public class BinaryTreeImp {
     public Note root;
 
     public void insert(long value) {
-
-
+    Note newNode=new Note(value);
+    Note current =root;
+    Note parent;
         if (root != null) {
             root.value = value;
             return;
+        }else{
+            while (true){
+   parent=current;
+   if(current.value>value){
+      current=current.leftNote;
+      if(current==null) {
+          parent.leftNote = newNode;
+          return;
+      }
+   }else {
+       current=current.rightNote;
+       if(current==null){
+           parent.rightNote=newNode;
+           return;
+       }
+   }
+            }
         }
 
 
