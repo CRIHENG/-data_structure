eebb8e27d7a020d89cd011be09b6f91ae4e76488
mr.c <crh1028vip@163.com>
2017-12-05 23:02:15 +0800

    二叉树

---
+++ DataStructure/src/Tree/BinaryTree/TestTree.java
@@ -0,0 +1,19 @@
+package Tree.BinaryTree;
+
+
+import org.junit.Test;
+
+public class TestTree {
+@Test
+    public void testTree01(){
+     BinaryTreeImp binaryTreeImp=new BinaryTreeImp();
+     binaryTreeImp.insert(10);
+     binaryTreeImp.insert(1);
+     binaryTreeImp.insert(3);
+     binaryTreeImp.insert(20);
+     System.out.println(binaryTreeImp.root.leftNote.value);
+     System.out.println(binaryTreeImp.root.leftNote.rightNote.value);
+     System.out.println(binaryTreeImp.root.rightNote.value);
+
+}
+}
