0b54373d3f83031c0fa5e85d63e376a71c5c93f3
CRH <crh1028vip@163.com>
2017-12-07 14:28:33 +0800

    动态规划学习

--- DataStructure/src/SeqList/MyJosePhus.java
+++ DataStructure/src/SeqList/MyJosePhus.java
@@ -6,25 +6,21 @@ public class MyJosePhus {
 
     /**
      * 顺序表实现约瑟夫环
-     *
-     *   题目：从第i个开始 每隔distance 删除一个元素 当剩最后一个元素返回该元素;
-     * */
+     * <p>
+     * 题目：从第i个开始 每隔distance 删除一个元素 当剩最后一个元素返回该元素;
+     */
     @Test
-    public void  josePhous(){
-        SeqList<Character> seq=new SeqList<Character>();
-        for(int i=0;i<5;i++){
-            seq.insert(i,(char)('A'+i));
+    public void josePhous() {
+        SeqList<Character> seq = new SeqList<Character>();
+        for (int i = 0; i < 5; i++) {
+            seq.insert(i, (char) ('A' + i));
         }
-        int distance=2;
-        int i=0;
-        while (seq.size()>1){
-            i=(i+distance-1)%seq.size();//?不懂
-
-            System.out.println(seq.remove(i));
-
+        int distance = 2;
+        int i = 0;
+        while (seq.size() > 1) {
+            i = (i + distance - 1) % seq.size();//?不懂
+            seq.remove(i);
         }
-       System.out.println(seq.get(0));
-
-
+        System.out.println(seq.get(0));
     }
 }
